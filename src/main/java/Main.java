f01fb2226c0eaf8faff9b006af107a939d4cc794
albertoiNET <correo@albertoi.net>
2018-03-14 21:22:03 +0100

    Some dumb stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ class Main {
         Singleton singleton1 = Singleton.getInstance();
         Singleton singleton2 = Singleton.getInstance();
 
+        // Do dumb stuff
         singleton1.sayHello();
         singleton2.sayHello();
     }
