f5f44adb9499227bd3d770992490a7f9182542fd
albertoiNET <correo@albertoi.net>
2018-03-17 19:53:49 +0100

    Added mockito unitary tests

---
+++ src/test/java/net/albertoi/patterdesign/singleton/TestSingleton.java
@@ -0,0 +1,31 @@
+package net.albertoi.patterdesign.singleton;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+public class TestSingleton {
+
+    @Mock
+    private Singleton singletonMock;
+
+    @Before
+    public void setup(){
+        singletonMock = Singleton.getInstance();
+
+    }
+
+    @Test
+    public void testGetInstanceNotNull(){
+        assertNotNull(singletonMock);
+    }
+
+    @Test
+    public void testGetInstanceUnique(){
+        assertEquals(singletonMock,Singleton.getInstance());
+
+    }
+}
